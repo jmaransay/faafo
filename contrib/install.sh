785d076001a0b9b61d2d77702df4897a37c752db
jmaransay <jmaransay@gmail.com>
2017-10-24 18:36:15 +0200

    Update install.sh

--- contrib/install.sh
+++ contrib/install.sh
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-hostname faafo-allinone;
+# hostname faafo-allinone;
  
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts;
 
